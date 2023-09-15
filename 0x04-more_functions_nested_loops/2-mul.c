bc945d0e17f74a8b830c7b5e5ba3141c577d7bdb
Nnajinwachi <nwachukwunnaji40@gmail.com>
2023-09-15 12:45:41 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ * @a: Entry point
+ * @b: Entry point
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	int d;
+
+	d = a * b;
+
+	return (d);
+}
