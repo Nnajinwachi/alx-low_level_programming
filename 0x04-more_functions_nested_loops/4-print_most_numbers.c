a8f33cc57d3db3f195b75622fa4b4dadffa63930
Nnajinwachi <nwachukwunnaji40@gmail.com>
2023-09-15 12:57:11 -0700

    most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Entry point
+ *
+ * Return: Always 0
+ */
+
+void print_most_numbers(void)
+{
+	int a = '0';
+
+	while (a <= '9')
+	{
+		if (a == '2' || a == '4')
+		{
+			a++;
+			continue;
+		}
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}
