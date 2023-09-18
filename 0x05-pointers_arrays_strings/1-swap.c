338725248dd2b122e963a5b379069ffea05da023
Nnajinwachi <nwachukwunnaji40@gmail.com>
2023-09-18 11:50:35 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - Entry point
+ * @a: value of integer
+ * @b: value of integer
+ * Return: Always 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int aux;
+
+	aux = *a;
+	*a = *b;
+	*b = aux;
+	
+}
