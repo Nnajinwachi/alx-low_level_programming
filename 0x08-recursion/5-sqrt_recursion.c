1aa7b598d3288363b263f2c088127f9d10b8b738
Nnajinwachi <nwachukwunnaji40@gmail.com>
2023-09-28 18:07:09 -0700

    all commit

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * square_root - determines the square root of
+ * @j: an input value
+ * @i: input value
+ * Return: returns the input val
+ */
+
+int square_root(int i, int j)
+{
+	if (j * j > i)
+	{
+		return (-1);
+	}
+	if (j * j == i)
+	{
+		return (j);
+	}
+	return (square_root(i, j + 1));
+}
+
+/**
+ * _sqrt_recursion - returns square root
+ * @n: input size
+ * Return: returns the natural square root of a number
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (square_root(n, 1));
+}
