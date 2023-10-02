24e2687f1bf73ebb70059d4b126f2a1680fbbdd3
Nnajinwachi <nwachukwunnaji40@gmail.com>
2023-10-02 16:43:49 -0700

    commit all

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: array to argc
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
